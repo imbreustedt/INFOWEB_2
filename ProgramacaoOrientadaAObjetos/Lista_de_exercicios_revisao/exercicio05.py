5c950a2484d335dc88c2a7dc562266b8d3f0ceba
imbreustedt <iuri.breustedt@gmail.com>
2026-03-26 18:11:21 +0000

    Conclusão do exercicio 05 da list ade revisão python

--- ProgramacaoOrientadaAObjetos/Lista_de_exercicios_revisao/exercicio05.py
+++ ProgramacaoOrientadaAObjetos/Lista_de_exercicios_revisao/exercicio05.py
@@ -3,8 +3,13 @@ class pais:
         self.nome = 0
         self.populacao = 0
         self.area = 0
-    def calculo(self):
+    def am2(self):
         return self.populacao / self.area
 
-x = dados() 
-x.nome = input('')
+x = pais() 
+x.nome = input('Diga o nome do pais:\n')
+x.populacao = float(input('Diga o numero da populacao\n'))
+x.area = float(input('Diga a area:\n'))
+print(x.populacao, x.area)
+y = x.am2()
+print(f"A densidade demográfica do {x.nome} é {y:.2f} hab/km²")
